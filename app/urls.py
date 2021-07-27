ec915ea2ed5a0cebee7b7858a8a8eb1310121842
WambiruL <wambirulorraine@gmail.com>
2021-07-27 15:11:54 +0300

    Add the user login page and its styling

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns=[
     url(r'^$', views.index,name='index'),
     url(r'register/',views.registerPage,name='register'),
+    url(r'login/',auth_views.LoginView.as_view(template_name='registration/login.html'),name='login'),
+    url(r'^logout/',auth_views.LogoutView.as_view(), {"next_page": '/login'}, name='logout',),
 ]
