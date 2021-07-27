5bd28f6e34f5a43fb560a263c7b45ee77521fb60
WambiruL <wambirulorraine@gmail.com>
2021-07-27 14:43:23 +0300

    create the registration form and its styling

--- app/views.py
+++ app/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.forms import forms
+from django.shortcuts import render,redirect
+from .models import *
+from .forms import *
+
 
 # Create your views here.
+def registerPage(request):
+    form=CreateUserForm()
+
+    if request.method=='POST':
+        form=CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            return redirect('login')
+
+    context={'form':form}
+    return render(request,'registration/registration.html',context)
+
+def index(request):
+    return render(request,'index.html')
--- chat/settings.py
+++ chat/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     #installed
-    'chat',
+    'app',
     'bootstrap4',
     'crispy_forms',
 ]
@@ -112,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
@@ -124,8 +124,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), ) 
 
+
+
+MEDIA_URL = '/media/' 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
