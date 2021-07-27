5055593fca82e476bccee327b1d1bcf156877af4
WambiruL <wambirulorraine@gmail.com>
2021-07-27 22:26:56 +0300

    Create message models

--- app/models.py
+++ app/models.py
@@ -2,7 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Room(models.Model):
-    name=models.CharField(max_lenght=500)
+    name=models.CharField(max_length=500)
 
     def __str__(self):
         return self.name
+post_date = models.DateTimeField(auto_now_add=True)
+class Message(models.Model):
+    message=models.CharField(max_length=200000)
+    date = models.DateTimeField(auto_now_add=True)
+    username=models.CharField(max_length=200)
+    room=models.ForeignKey(Room,related_name='message')
